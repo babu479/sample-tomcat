cd0c3b6adf584d2e3917063862ef1ccf2f5a7a7a
Greg Cox <gregcox11@gmail.com>
2017-01-31 16:35:30 -0600

    add curl/grep to check content not just status code for verification

--- scripts/basic_health_check.sh
+++ scripts/basic_health_check.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+keyword="Bespoke"
+
 for i in `seq 1 10`;
 do
   HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80`
   if [ "$HTTP_CODE" == "200" ]; then
-    echo "Successfully pulled root page."
-    exit 0;
+    if (curl -s "http://localhost:80" | grep "$keyword")
+            # if the keyword is in the conent
+        echo "Successfully pulled root page."
+        exit 0;
+    fi
+
   fi
   echo "Attempt to curl endpoint returned HTTP Code $HTTP_CODE. Backing off and retrying."
   sleep 10
